c960da01788df31da80cf7e066111c896ebbecc0
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-20 13:39:43 -0400

    App & styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import { BrowserRouter } from "react-router-dom";
 import PixlyApi from './api';
 import RoutesList from "./RoutesList";
 import NavBar from "./NavBar";
+import "./App.css";
+
+/**
+ * App Component
+ *
+ * The main component of the Pix.ly application. Initializes and manages the state
+ * for image data, loading state, search visibility, and current search term. Also
+ * provides an interface for fetching images from the API, uploading images,
+ * and resetting the app state. Navigation is handled by the BrowserRouter.
+ *
+ * State:
+ *  - isLoading: tracks if the application is in a loading state.
+ *  - imagesData: array of images fetched from the PixlyApi.
+ *  - showSearch: toggles visibility of the search form.
+ *  - currSearchTerm: current search term to filter images.
+ *
+ * Props:
+ *  - None
+ *
+ * App -> NavBar
+ * App -> RoutesList
+ */
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [imagesData, setImagesData] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
   const [currSearchTerm, setCurrSearchTerm] = useState("");
 
   useEffect(function loadImageUrls() {
@@ -46,7 +70,8 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <NavBar resetAppState={() => setCurrSearchTerm("")} />
+        <NavBar resetAppState={() => setCurrSearchTerm("")}
+                setShowSearch={setShowSearch} />
         {imagesData &&
           <RoutesList
             handleUpload={handleUpload}
@@ -54,6 +79,8 @@ function App() {
             handleSearch={setCurrSearchTerm}
             currSearchTerm={currSearchTerm}
             setCurrSearchTerm={setCurrSearchTerm}
+            showSearch={showSearch}
+            setShowSearch={setShowSearch}
           />
         }
       </div>
